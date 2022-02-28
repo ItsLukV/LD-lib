b4e70076fd98de9da64ff0d4601d2fbf9aa0f2f1
dendersen <dendersen@endersen.dk>
2022-02-28 14:44:41 +0100

    ?

--- LD-lib.js
+++ LD-lib.js
@@ -90,3 +90,93 @@ function randomColor() {
   result = "rgb" + "(" + colorArray + ")";
   return result;
 }
+
+const graffSaverArray_No_Touch__For_Real__ = []
+
+function grafferLD (Gwidth, Gheight, locationX, locationY, calculations, operations,restart){
+  let y, x, runNumber, density, colR, colG, colB, action
+
+  if (reset) for (let i = 0; i<graffSaverArray_No_Touch__For_Real__.length; i++){
+  graffSaverArray_No_Touch__For_Real__ [i] = undefined
+  }else if (graffSaverArray_No_Touch__For_Real__[-1]){
+    let j = 0
+    graffSaverArray_No_Touch__For_Real__ [j] = y
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = x
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = runNumber
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = density
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = colR
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = colG
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = colB
+    j++
+    graffSaverArray_No_Touch__For_Real__ [j] = action
+    j++
+  }
+
+  function pointDraw(id,CC){ //makes a point showing where is currently being drawn
+    if(CC) y = calculations [id]()
+    strokeWeight(LI/3)
+    point(x+Gwidth/2+locationX,-y+Gheight/2+locationY)
+    
+    //draws lines on x and y for drawn point
+    stroke(colR, colG, colB)
+    point(x+Gwidth/2+locationX,height/2+locationY)
+    point(width/2+locationX,-y+Gheight/2+locationY)
+    strokeWeight(LI)
+    stroke(0)
+  }
+  function pointMove(){
+    if(x<width/2){
+      if(runNumber>30){
+      x+=width/(runNumber*density*(height/50))
+      }else{
+        x+=width/(runNumber*density*(height/500))
+      }
+    }else{
+      x=-width/2
+      colorControl()
+    }
+  }
+  function colorControl() {
+    runNumber++
+    print(runNumber)
+    if (action < 256) {
+      colR -= 32
+      colB += 32
+      action += 32
+    } else if (action < 512) {
+      colB -= 32
+      colG += 32
+      action += 32
+    } else if (action < 768) {
+      colG -= 32
+      colR += 32
+      action += 32
+    } else {
+      action = 0
+    }
+    density += 10
+  }
+  function pointDrawMulty(id){
+    let TT =true
+    for(let i  = 0; i!=2;i++){
+      y = calculate[id](TT)
+      pointDraw(0,false)
+      TT=false
+    }
+  }
+  
+  for(let i = 0; i<operations.length; i++){
+    if(operations [i] == true){
+      pointDrawMulty(i)
+    }else{
+      pointDraw(i,true)
+    }
+    pointMove()
+  }
+}
